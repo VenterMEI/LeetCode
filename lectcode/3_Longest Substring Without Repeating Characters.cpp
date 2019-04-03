f76d908f71ae9170f4b982db65cba9d0da663597
sudamj <1074715353@qq.com>
2019-04-03 18:25:36 +0800

    sliding_window_without_hash 99.84% 100.00%

--- lectcode/3_Longest Substring Without Repeating Characters.cpp
+++ lectcode/3_Longest Substring Without Repeating Characters.cpp
@@ -1,12 +1,17 @@
 /*
-Runtime: 20 ms, faster than 97.40% of C++ online submissions for Longest Substring Without Repeating Characters.
-Memory Usage: 14.8 MB, less than 79.83% of C++ online submissions for Longest Substring Without Repeating Characters.
+Runtime: 12 ms, faster than 99.84% of C++ online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 8.9 MB, less than 100.00% of C++ online submissions for Longest Substring Without Repeating Characters.
+*/
+
+/*
+总结：
+subsequence 可以不连续 & substring 连续
 */
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         string::const_iterator l=s.cbegin(),r=s.cbegin();
-        string::difference_type max_dist = r-l;
+        string::difference_type max_dist = 0;
         while(r!=s.cend()){
             int is_same = 0;
             auto l_copy = l;
